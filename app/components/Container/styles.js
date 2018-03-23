dd3a38fc515c511a75ab59dc9d7d009b33dcc009
Kyle Deak <kdeak1210@gmail.com>
2018-03-23 18:09:58 -0400

    Created Home Screen & Container Component
    The Container component is used to render children components inside of a styled container. It takes up the whole screen, aligning items to the center both vertical & horizontal, and is styled with the primaryBlue color defined in the global stylesheet at 'app/index.js'

---
+++ app/components/Container/styles.js
@@ -0,0 +1,14 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+/** Take up the whole screen (flex: 1)
+ *  Center align vertical and horizontally
+ *  Use the $primaryBlue global variable created in EStyleSheet.build({})
+ */
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '$primaryBlue',
+  },
+});
