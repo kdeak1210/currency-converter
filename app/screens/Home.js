3eb23a65d8019b4b138afc1d306aa774f7bac6a3
Kyle Deak <kdeak1210@gmail.com>
2018-03-24 14:24:47 -0400

    Created InputWithButton component
    This component is used twice on the Home screen, and consists of a View wrapping a Button on the left, and TextInput on the right. The Button's text shows a currency, and clicking it should enable the user to pick a new currency. The TextInput is to enter a numeric amount of the selected currency. The second component is passed an editable=false prop, because the user shouldnt be able to enter text there as it is view only

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  };
+
+  handleTextChange = (text) => {
+    console.log('Text Changed: ', text);
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          onChangeText={this.handleTextChange}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          underlineColorAndroid="transparent"
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;
