356e585aeef131e4e9af227efe271dd79797027b
Kyle Deak <kdeak1210@gmail.com>
2018-03-26 19:57:55 -0400

    Options Screen
    The options screen uses the previously created ListItem components to list a couple options. The plan is to allow seleciton between different Themes, and access Fixer.io, the Currency converting API that will be used later on

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 // import Home from './screens/Home';
-import CurrencyList from './screens/CurrencyList';
+// import CurrencyList from './screens/CurrencyList';
+import Options from './screens/Options';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -16,4 +17,4 @@ EStyleSheet.build({
   // $outline: 1
 });
 
-export default () => <CurrencyList />;
+export default () => <Options />;
