d1f87395bf242ba73607e02578350a72c34fb99f
Kyle Deak <kdeak1210@gmail.com>
2018-03-27 19:24:40 -0400

    Updated currency reducer for currency change actions
    On currency change actions (base & quote), the store is updated with the newly selected baseCurrency or quoteCurrency, along with a new conversions object, set to fetch the new data from Fixer.io API. But, if a conversions object already exists for that Base currency, then the API fetch is not performed again and the previous data is reused

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -58,27 +58,52 @@ const initialState = {
   }
 };
 
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: '',
+    rates: {}
+  };
+
+  if (state.conversions[action.currency]) {
+    conversion = state.conversions[action.currency];
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversion
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SWAP_CURRENCY:
-      console.log('SWAP_CURRENCY');
+      // console.log('SWAP_CURRENCY');
       return {
         ...state,
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency
       };
     case CHANGE_CURRENCY_AMOUNT:
-      console.log(`CHANGE_CURRENCY_AMOUNT: ${JSON.stringify(action)}`);
+      // console.log(`CHANGE_CURRENCY_AMOUNT: ${JSON.stringify(action)}`);
       return {
         ...state,
         amount: action.amount || 0
       };
     case CHANGE_BASE_CURRENCY:
-      console.log(`CHANGE_BASE_CURRENCY: ${action.currency}`);
-      return state;
+      // console.log(`CHANGE_BASE_CURRENCY: ${action.currency}`);
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action)
+      };
     case CHANGE_QUOTE_CURRENCY:
-      console.log(`CHANGE_QUOTE_CURRENCY: ${action.currency}`);
-      return state;
+      // console.log(`CHANGE_QUOTE_CURRENCY: ${action.currency}`);
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action)
+      };
     default:
       return state;
   }
